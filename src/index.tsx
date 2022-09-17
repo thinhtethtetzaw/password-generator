b18291b024aa70eee34039d63472b61752500857
thinhtethtetzaw778 <thinhtethtetzaw778@gmail.com>
2022-09-17 14:28:03 +0630

    Changed Favicon and title

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Helmet } from "react-helmet";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Helmet>
+			<meta charSet="utf-8" />
+			<title>Password Generator</title>
+			<link rel="canonical" href="http://mysite.com/example" />
+		</Helmet>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
